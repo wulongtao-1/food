81807de982fa93fe526fa0aca739671958cd59f7
2302606093 <2302606093@qq.com>
2019-09-12 10:18:29 +0800

    sx

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter= require('./routes/register');
 var dianwaimaiRouter= require('./routes/dianwaimai');
+var kaifangpingtaiRouter= require('./routes/kaifangpingtai');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register',registerRouter);
 app.use('/dianwaimai',dianwaimaiRouter);
+app.use('/kaifangpingtai',kaifangpingtaiRouter);
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
