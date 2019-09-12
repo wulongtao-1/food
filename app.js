83efc0cc73a65525a8bda1acfb47bd1fe67b050c
JasonGong2333 <892584933@qq.com>
2019-09-12 10:00:23 +0800

    success

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter= require('./routes/register');
+var dianwaimaiRouter= require('./routes/dianwaimai');
 
 var app = express();
 
@@ -23,7 +24,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register',registerRouter);
-
+app.use('/dianwaimai',dianwaimaiRouter);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
