a3c493773687feb3b242ed5c08cab401c0d0272d
JasonGong2333 <892584933@qq.com>
2019-09-16 16:37:07 +0800

    9.16gz

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var registerRouter= require('./routes/register');
 var dianwaimaiRouter= require('./routes/dianwaimai');
+var shangouRouter= require('./routes/shangou');
 
 var app = express();
 
@@ -25,7 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/register',registerRouter);
 app.use('/dianwaimai',dianwaimaiRouter);
-// catch 404 and forward to error handler
+app.use('/shangou',shangouRouter);
 app.use(function(req, res, next) {
   next(createError(404));
 });
